d3fdb903b1ee6eec8ef380bad3bd0464719126f6
Haelwenn (lanodan) Monnier <contact@hacktivis.me>
2023-09-08 10:11:49 +0200

    Use builtin node:test instead of npm:tap dependency

--- test/test.js
+++ test/test.js
@@ -1,17 +1,16 @@
-const abbrev = require('../')
-const t = require('tap')
+const abbrev = require('abbrev')
+const t = require('node:test')
+const assert = require('assert');
 const util = require('util')
 
 function test (list, expect) {
-  let actual = abbrev(list)
-  t.same(actual, expect,
-    'abbrev(' + util.inspect(list) + ') === ' + util.inspect(expect) + '\n' +
-    'actual: ' + util.inspect(actual))
+  t.test(() => {
+    let actual = abbrev(list);
+    assert.deepStrictEqual(actual, expect);
 
-  actual = abbrev(...list)
-  t.same(abbrev(...list), expect,
-    'abbrev(' + list.map(JSON.stringify).join(',') + ') === ' + util.inspect(expect) + '\n' +
-    'actual: ' + util.inspect(actual))
+    actual = abbrev(...list);
+    assert.deepStrictEqual(actual, expect);
+  });
 }
 
 t.test('single string input', t => {
@@ -61,5 +60,5 @@ test(['a', 'ab', 'abc', 'abcd', 'abcde', 'acde'].reverse(),
     acde: 'acde',
   })
 
-t.notOk([].abbrev)
-t.notOk({}.abbrev)
+t.test(() => assert(![].abbrev))
+t.test(() => assert(!{}.abbrev))
